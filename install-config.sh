76c9556989da07246bb35c4b039a3d53ce5a9b9e
ahmadrawwagah <ahmadrawwagah@gmail.com>
2024-12-03 21:20:35 -0600

    added alacritty config

--- install-config.sh
+++ install-config.sh
@@ -1,10 +1,14 @@
+echo "Installing Alacritty Config"
+cp ./.alacritty.toml ~/
+echo "Installing Nvim and Tmux Config"
 mkdir -p ~/.config/nvim
 cp ./tmux.conf ~/
 cp ./init.lua ~/.config/nvim/
 rm -rf ~/.config/tmux/plugins
 mkdir -p ~/.config/tmux/plugins/
+echo "Installing Tmux Plugins"
 git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/
 git clone https://github.com/tmux-plugins/tmux-cpu ~/.config/tmux/plugins/tmux-cpu
 git clone https://github.com/tmux-plugins/tmux-battery ~/.config/tmux/plugins/tmux-battery
 
-#need to install clangd,pylsp,lualsp,fzf,bat,ripgrep,the_silver_searcher,perl,universal-ctags
+echo "Need to install clangd, pylsp, lualsp, fzf, bat, ripgrep, the_silver_searcher, perl, universal-ctags"
